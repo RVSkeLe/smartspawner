e9d414b6bfb14cee1c16b67f0c90698047f76ffd
Tien Thanh <notnighter@gmail.com>
2025-12-22 14:16:57 +0700

    feat: restore and enable integrations for zShop and IridiumSkyblock

--- core/src/main/java/github/nighter/smartspawner/hooks/protections/CheckStackBlock.java
+++ core/src/main/java/github/nighter/smartspawner/hooks/protections/CheckStackBlock.java
@@ -23,7 +23,7 @@ public class CheckStackBlock {
         if (integrationManager.isHasSimpleClaimSystem() && !SimpleClaimSystem.canPlayerStackClaimBlock(player, location))
             return false;
         if (integrationManager.isHasMinePlots() && !MinePlots.canPlayerStackBlock(player, location)) return false;
-//        if (integrationManager.isHasIridiumSkyblock() && !IridiumSkyblock.canPlayerStackBlock(player, location)) return false;
+        if (integrationManager.isHasIridiumSkyblock() && !IridiumSkyblock.canPlayerStackBlock(player, location)) return false;
         if (integrationManager.isHasPlotSquared() && !PlotSquared.canInteract(player, location)) return false;
         if (integrationManager.isHasResidence() && !Residence.canStack(player, location)) return false;
         return !integrationManager.isHasRedProtect() || RedProtectAPI.canPlayerStackClaimBlock(player, location);
